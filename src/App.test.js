ba3666e93454610dce598bc648f7c990724f0d29
Issac Moreno <issaxavier909@gmail.com>
2020-02-13 15:22:34 -0800

    MVP

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { render } from "@testing-library/react";
+
+
 import App from "./App";
 
 test("renders App without crashing", () => {
-  render(<App />);
+  const page = render(<App />);
+  expect(page).toBeVisible;
 });
+
+
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -16,24 +16,28 @@ const ContactForm = () => {
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            id = 'fistName'
             name="firstName"
             placeholder="bill"
             ref={register({ required: true, maxLength: 3 })}
+            data-testid = 'first-name'
           />
           {errors.firstName && (
-            <p>Looks like there was an error: {errors.firstName.type}</p>
+            <p data-testid = 'first-name-error'>Looks like there was an error: {errors.firstName.type}</p>
           )}
         </div>
 
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            id = 'lastName'
             name="lastName"
             placeholder="luo"
             ref={register({ required: true })}
+            data-testid = 'last-name'
           />
           {errors.lastName && (
-            <p>Looks like there was an error: {errors.lastName.type}</p>
+            <p data-testid = 'last-name-error'>Looks like there was an error: {errors.lastName.type}</p>
           )}
         </div>
 
@@ -41,21 +45,21 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input id = 'email' name="email" ref={register({ required: true })} data-testid ='email' />
           {errors.email && (
-            <p>Looks like there was an error: {errors.email.type}</p>
+            <p data-testid = 'email-error'>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea name="message" ref={register({ required: false })} data-testid = 'message'/>
         </div>
         {data && (
-          <pre style={{ textAlign: "left", color: "white" }}>
+          <pre data-testid = 'data-displayed' style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input data-testid = 'submit-button' type="submit" />
       </form>
     </div>
   );
